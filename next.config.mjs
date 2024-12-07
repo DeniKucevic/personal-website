ceb7926eab3ed59a8a73601d04d78021368c167e
brixi <denikucevic@gmail.com>
2024-12-08 00:18:04 +0100

    fix mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
+import mdx from "@next/mdx";
+import createNextIntlPlugin from "next-intl/plugin";
+import createMDX from "@next/mdx";
 
-const withMDX = mdx({
-    extension: /\.mdx?$/,
-    options: { },
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
 export default withNextIntl(withMDX(nextConfig));
